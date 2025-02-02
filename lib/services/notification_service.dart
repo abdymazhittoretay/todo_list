2da1d792b9779f131c5767c5d509c39c50e7edbb
Toretay <abdymvzhit@gmail.com>
2025-02-02 22:50:25 +0500

    added initialization for notifications dep

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,3 +1,26 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 class NotificationService {
-  
+  final FlutterLocalNotificationsPlugin notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  bool _isInit = false;
+
+  bool get isInit => _isInit;
+
+  Future init() async {
+    if (_isInit) {
+      return;
+    }
+
+    const initSettings = InitializationSettings(
+        android: AndroidInitializationSettings("@mipmap/ic_launcher"),
+        iOS: DarwinInitializationSettings(
+          requestAlertPermission: true,
+          requestBadgePermission: true,
+          requestSoundPermission: true,
+        ));
+
+    await notificationsPlugin.initialize(initSettings);
+  }
 }
